521d3c5f77957861339913aff0e0661b1d60a659
JamesMcLennan <jamesamclennan@gmail.com>
2017-07-27 21:00:48 +1000

    Implemented Routes to essential pages

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+//Home page View Route
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Request Service View Route
+Route::get('requestservice', function() {
+    return view('requestservice');
+});
+
+//Track Progress View Route
+Route::get('trackprogress', function() {
+    return view('trackprogress');
+});
+
+//Report an Issue View Route
+Route::get('reportissue', function() {
+    return view('reportissue');
+});
+
+
