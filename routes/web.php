6030173a52d5bd7c73149fe6cbdbc219b5912605
JamesMcLennan <jamesamclennan@gmail.com>
2017-07-28 11:08:16 +1000

    Add subfolder routing to views

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,26 @@
 
 //Home page View Route
 Route::get('/', function () {
-    return view('welcome');
+    return View::make('pages.welcome');
+});
+
+Route::get('welcome', function () {
+    return View::make('pages.welcome');
 });
 
 //Request Service View Route
 Route::get('requestservice', function() {
-    return view('requestservice');
+    return View::make('pages.requestservice');
 });
 
 //Track Progress View Route
 Route::get('trackprogress', function() {
-    return view('trackprogress');
+    return View::make('pages.trackprogress');
 });
 
 //Report an Issue View Route
 Route::get('reportissue', function() {
-    return view('reportissue');
+    return View::make('pages.reportissue');
 });
 
 
