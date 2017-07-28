f18b2e45eef57cec8b1376c23eb47932494ce823
JamesMcLennan <jamesamclennan@gmail.com>
2017-07-28 14:12:51 +1000

    Added page icon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,7 +4,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Page Title & Icon -->
         <title>RMIT Service Now</title>
+        <link rel="icon" href="{{URL::asset('images/rmitlogo.jpg')}}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
