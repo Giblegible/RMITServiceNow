82844a602ec1cdfc280a57e69913e5ab0bcceff2
JamesMcLennan <jamesamclennan@gmail.com>
2017-07-29 09:59:01 +1000

    Added margin to filter divs as well as bootstrap columns

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,21 +10,28 @@
     </div>
 
     <div id="filter" class="col-md-12 text-center">
-        <div id="critical" class="col-md-4">
-            <h4>Critical</h4>
-            <p>Is your issue critical?</p>
-            <p>Call our IT Help Team on 9999 9999</p>
+        <div id="critical" class="col-md-4 panel panel-danger">
+            <h4 class="panel-heading">Critical</h4>
+            <div class="panel-body">
+                <p>Is your issue critical?</p>
+                <p>Call our IT Help Team on 9999 9999</p>
+            </div>
         </div>
 
-        <div id="non-critical" class="col-md-4">
-            <h4>Non Critical</h4>
-            <p>Is your issue non-critical?</p>
-            <a href="{{ URL::to('reportissue') }}">Report your issue here</a>
+        <div id="non-critical" class="col-md-4 panel panel-warning">
+            <h4 class="panel-heading">Non Critical</h4>
+            <div class="panel-body">
+                <p>Is your issue non-critical?</p>
+                <p><a href="{{ URL::to('reportissue') }}">Report your issue here</a></p>
+            </div>
         </div>
 
-        <div id="general-advise" class="col-md-4">
-            <h4>General Advise</h4>
-            <p>Just need some basic help? Try reading through our FAQ's for assistance!</p>
+        <div id="general-advise" class="col-md-4 panel panel-info">
+            <h4 class="panel-heading">General Advise</h4>
+            <div class="panel-body">
+                <p>Just need some basic help?</p>
+                <p>Try reading through our FAQ's for assistance!</p>
+            </div>
         </div>
 
     </div>
