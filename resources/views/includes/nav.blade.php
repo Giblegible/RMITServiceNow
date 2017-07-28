c9465f335c0d5698eb5b5d13ea6d6b9a500a7ab1
JamesMcLennan <jamesamclennan@gmail.com>
2017-07-28 17:48:29 +1000

    Implemented standard navigation bar

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,6 +1,13 @@
-    <div class="links">
-        <a href="{{ URL::to('home') }}">Home</a>
-        <a href="{{ URL::to('requestservice') }}">Request Service</a>
-        <a href="{{ URL::to('trackprogress') }}">Track Progress</a>
-        <a href="{{ URL::to('reportissue') }}">Report Issue</a>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">RMIT Service Now</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="{{ URL::to('home') }}">Home</a></li>
+            <li><a href="{{ URL::to('requestservice') }}">Request Service</a></li>
+            <li><a href="{{ URL::to('trackprogress') }}">Track Progress</a></li>
+            <li><a href="{{ URL::to('reportissue') }}">Report Issue</a></li>
+        </ul>
     </div>
+</nav>
