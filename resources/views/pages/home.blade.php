6955f07edb4888fccf358bf865abcd7bfde7c005
JamesMcLennan <jamesamclennan@gmail.com>
2017-07-31 18:21:54 +1000

    Recreated home page to match theme

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,110 +5,74 @@
 @include('includes/header')
 <div class="">
     @include('includes/login')
-    <div id="welcomeMessage">
-        
-    </div>
 
-    <div id="filter" class="col-md-12 text-center">
-        <div id="critical" class="col-md-4 panel panel-danger">
-            <h4 class="panel-heading">Critical</h4>
-            <div class="panel-body">
-                <p>Is your issue critical?</p>
-                <p>Call our IT Help Team on 9999 9999</p>
+    <!-- Page Content -->
+    <div class="container">
+
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-8">
+                <h2>What We Do</h2>
+                <p>The RMIT IT Support team is a group of skilled individuals who's primary role is to assist and educate customers as the first point
+                    of contact. We aim to solve a wide range of issues and problems that our staff or students cannot solve.</p>
+            </div>
+            <div class="col-sm-4">
+                <h2>Contact Us</h2>
+                <address>
+                    <strong>RMIT ITS</strong>
+                    <br>Building 88 Level 10
+                    <br>RMIT City campus
+                    <br>440 Elizabeth Street
+                    <br>Melbourne
+                    <br>
+                </address>
+                <address>
+                    <abbr title="Phone">P:</abbr>+61 3 9925 8600
+                    <br>
+                    <abbr title="Email">E:</abbr> <a href="mailto:#">its.reception@rmit.edu.au</a>
+                </address>
             </div>
         </div>
+        <!-- /.row -->
 
-        <div id="non-critical" class="col-md-4 panel panel-warning">
-            <h4 class="panel-heading">Non Critical</h4>
-            <div class="panel-body">
-                <p>Is your issue non-critical?</p>
-                <p><a href="{{ URL::to('reportissue') }}">Report your issue here</a></p>
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-4 filter">
+                <i class="fa fa-exclamation-triangle icon" aria-hidden="true" id="critical"></i>
+                <h2>Critical</h2>
+                <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
+            </div>
+            <div class="col-sm-4 filter">
+                <i class="fa fa-exclamation icon" aria-hidden="true" id="non-critical"></i>
+                <h2>Non Critical</h2>
+                <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+            </div>
+            <div class="col-sm-4 filter">
+                <i class="fa fa-question icon" aria-hidden="true" id="general"></i>
+                <h2>General Advise</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
             </div>
         </div>
-
-        <div id="general-advise" class="col-md-4 panel panel-info">
-            <h4 class="panel-heading">General Advise</h4>
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                FAQ's
+            </div>
             <div class="panel-body">
-                <p>Just need some basic help?</p>
-                <p>Try reading through our FAQ's for assistance!</p>
+                <ul>
+                    <li>
+                        Blah
+                        <ul>
+                            <li>
+                                Bluh
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
-
-    </div>
-    <div id="about" class="text-center col-md-12">
-        <h3>About RMIT ITS Team</h3>
-        <div class="col-md-8 col-md-offset-2">
-            <img src="{{ URL::to('images/RMITAcademic.jpg') }}" class="img-responsive"/>
-            <p>The RMIT IT Support team is a group of skilled individuals who's primary role is to assist and educate customers as the first point
-            of contact. We aim to solve a wide range of issues and problems that our staff or students cannot solve.</p>
-        </div>
-    </div>
-
-    <div id="howto" class="text-center">
-        <h3>How does the RMIT Ticketing System Work?</h3>
-        <p>It works by flow</p>
-    </div>
-
-    <div id="faqs">
-        <h3>FAQ's</h3>
-        <ul>
-            <li>
-                How long will it take?
-                <ul>
-                    <li>
-                        As long as a piece of string
-                    </li>
-                </ul>
-            </li>
-            <li>
-                How long will it take?
-                <ul>
-                    <li>
-                        As long as a piece of string
-                    </li>
-                </ul>
-            </li>
-            <li>
-                How long will it take?
-                <ul>
-                    <li>
-                        As long as a piece of string
-                    </li>
-                </ul>
-            </li>
-            <li>
-                How long will it take?
-                <ul>
-                    <li>
-                        As long as a piece of string
-                    </li>
-                </ul>
-            </li>
-            <li>
-                How long will it take?
-                <ul>
-                    <li>
-                        As long as a piece of string
-                    </li>
-                </ul>
-            </li>
-            <li>
-                How long will it take?
-                <ul>
-                    <li>
-                        As long as a piece of string
-                    </li>
-                </ul>
-            </li>
-            <li>
-                How long will it take?
-                <ul>
-                    <li>
-                        As long as a piece of string
-                    </li>
-                </ul>
-            </li>
-        </ul>
+        <!-- /.row -->
     </div>
 
 </div>
