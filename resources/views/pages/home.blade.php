c8bc332d41b05e1481f0aa6b3f03b9172fdaa5e3
JamesMcLennan <jamesamclennan@gmail.com>
2017-07-31 22:43:28 +1000

    Updated header image

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,13 +2,6 @@
 @section('title', 'Home')
 @section('content')
     <header class="business-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="tagline">RMIT Service Now</h1>
-                </div>
-            </div>
-        </div>
     </header>
     <div>
     @include('includes/login')
