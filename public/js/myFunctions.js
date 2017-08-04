700388f914635d9ee954ba5036443e22a0d0c81a
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-04 21:03:02 +1000

    removed software "other" option

--- public/js/myFunctions.js
+++ public/js/myFunctions.js
@@ -2,14 +2,12 @@ $(document).ready(function() {
     //Hide computer hardware option
     $('#computerHardware').hide();
     $('#computerSoftware').hide();
-    $('#computerSoftwareOther').hide();
 
     $('#serviceArea').on('change', function() {
         var service = $("#serviceArea option:selected").text();
         if(service === "Computer (Hardware)") {
             $('#computerHardware').show();
             $('#computerSoftware').hide();
-            $('#computerSoftwareOther').hide();
         }
         else if(service === "Computer (Software)") {
             $('#computerSoftware').show();
@@ -18,16 +16,6 @@ $(document).ready(function() {
         else {
             $('#computerHardware').hide();
             $('#computerSoftware').hide();
-        }
-    });
-
-    $('#computerSoftware').on('change', function() {
-        var option = $("#computerSoftware option:selected").text();
-        if(option === "Other") {
-            $('#computerSoftwareOther').show();
-        }
-        else {
-            $('#computerSoftwareOther').hide();
         }
     });
 });
