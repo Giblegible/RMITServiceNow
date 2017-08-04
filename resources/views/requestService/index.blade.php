c784223c515250b7e947cdfd4f8f9d2c3781c200
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-04 21:02:17 +1000

    Index for requestService
    Index currently displays all services

---
+++ resources/views/requestService/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layout.master')
+@section('title', 'View Tickets')
+@section('content')
+    <div class="container">
+        <h2>Tickets</h2>
+        <hr>
+        <div class="row">
+            <div class="col-md-2">
+                <h3>ID</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Service Area</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Work Area</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Description</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Hardware Type</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Software Type</h3>
+            </div>
+        </div>
+        <hr>
+        @foreach($tickets as $ticket)
+            <div class="row">
+                <div class="col-md-2">
+                    <a href="{{ URL::to('requestService', $ticket->id) }}">{{ $ticket->id}}</a>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->serviceArea}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->workArea}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->problemDescription}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->hardwareType}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->softwareType}}</p>
+                </div>
+            </div>
+            <hr>
+        @endforeach
+    </div>
+
+@endsection
