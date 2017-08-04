f22093fc90bb0aba8c6c3dfe11de1735f550487d
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-04 12:41:21 +1000

    Hide elements updated

--- public/js/myFunctions.js
+++ public/js/myFunctions.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
     //Hide computer hardware option
     $('#computerHardware').hide();
+    $('#computerSoftware').hide();
+    $('#computerSoftwareOther').hide();
 
     $('#serviceArea').on('change', function() {
         var service = $("#serviceArea option:selected").text();
         if(service === "Computer (Hardware)") {
             $('#computerHardware').show();
+            $('#computerSoftware').hide();
+            $('#computerSoftwareOther').hide();
+        }
+        else if(service === "Computer (Software)") {
+            $('#computerSoftware').show();
+            $('#computerHardware').hide();
         }
         else {
             $('#computerHardware').hide();
+            $('#computerSoftware').hide();
         }
-    })
+    });
+
+    $('#computerSoftware').on('change', function() {
+        var option = $("#computerSoftware option:selected").text();
+        if(option === "Other") {
+            $('#computerSoftwareOther').show();
+        }
+        else {
+            $('#computerSoftwareOther').hide();
+        }
+    });
 });
