dfab415520a52e6f5b66372210bc95f3609b0711
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-04 21:01:30 +1000

    Updated temporary format

--- resources/views/requestService/show.blade.php
+++ resources/views/requestService/show.blade.php
@@ -2,52 +2,30 @@
 @section('title', 'View Tickets')
 @section('content')
     <div class="container">
-        <h2>Tickets</h2>
+        <h2>{{ $ticket->id }}</h2>
         <hr>
+
         <div class="row">
             <div class="col-md-2">
-                <h3>Service Area</h3>
+                <p>{{ $ticket->id}}</p>
             </div>
             <div class="col-md-2">
-                <h3>Work Area</h3>
+                <p>{{ $ticket->serviceArea}}</p>
             </div>
             <div class="col-md-2">
-                <h3>Description</h3>
+                <p>{{ $ticket->workArea}}</p>
             </div>
             <div class="col-md-2">
-                <h3>Hardware Type</h3>
+                <p>{{ $ticket->problemDescription}}</p>
             </div>
             <div class="col-md-2">
-                <h3>Software Type</h3>
+                <p>{{ $ticket->hardwareType}}</p>
             </div>
             <div class="col-md-2">
-                <h3>Comments</h3>
+                <p>{{ $ticket->softwareType}}</p>
             </div>
         </div>
         <hr>
-        @foreach($tickets as $ticket)
-            <div class="row">
-                <div class="col-md-2">
-                    <p>{{ $ticket->serviceArea}}</p>
-                </div>
-                <div class="col-md-2">
-                    <p>{{ $ticket->workArea}}</p>
-                </div>
-                <div class="col-md-2">
-                    <p>{{ $ticket->problemDescription}}</p>
-                </div>
-                <div class="col-md-2">
-                    <p>{{ $ticket->hardwareType}}</p>
-                </div>
-                <div class="col-md-2">
-                    <p>{{ $ticket->softwareType}}</p>
-                </div>
-                <div class="col-md-2">
-                    <p>{{ $ticket->comments}}</p>
-                </div>
-            </div>
-            <hr>
-        @endforeach
     </div>
 
 @endsection
