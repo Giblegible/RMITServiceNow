546f27d1be01f1d7fcede48312dedc50fe48c221
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-04 21:03:38 +1000

    Updated CustomerQueryController with CRUD functions

--- app/Http/Controllers/CustomerQueryController.php
+++ app/Http/Controllers/CustomerQueryController.php
@@ -1,21 +1,21 @@
 <?php
 namespace App\Http\Controllers;
+use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\CustomerQuery;
 
 class CustomerQueryController extends Controller
 {
-    //
+    public function index()
+    {
+        $tickets = CustomerQuery::all();
+        return view('requestService.index', compact('tickets'));
+    }
+
     public function create()
     {
         $query = new CustomerQuery;
         return view('requestService.create', ['query' => $query ]);
-    }
-
-    public function show()
-    {
-        $tickets = CustomerQuery::all();
-        return view('requestService.show')->with('tickets', $tickets);
     }
 
     public function store(Request $request)
@@ -29,5 +29,41 @@ class CustomerQueryController extends Controller
         CustomerQuery::create($request->all());
         return redirect()->route('requestService.create') ->with('success','Service requested
             successfully. Our team will be in touch within 72 hours.');
+    }
+
+    public function show($id) {
+        $ticket = CustomerQuery::find($id);
+        return view('requestService.show', compact('ticket'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
