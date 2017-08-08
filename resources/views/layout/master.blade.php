99d6eb2d356570bed4c711de6839a6d6918c59e8
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-08 10:02:21 +1000

    Updated imported font

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -10,7 +10,7 @@
     <link rel="icon" href="{{URL::asset('images/rmitlogo.jpg')}}">
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
 
     <!-- Icons -->
     <link rel="stylesheet" href="{{ URL::asset('css/FontAwesome/css/font-awesome.min.css') }}">
