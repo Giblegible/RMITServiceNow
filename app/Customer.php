38f669c98c7c80d45679c6caee431208d6ab4fdd
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-10 10:25:30 +1000

    Customer model

---
+++ app/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'name', 'email', 'phoneNum', 'program',
+    ];
+
+    public function customer_queries() {
+        return $this->hasMany('App\CustomerQuery');
+    }
+}
