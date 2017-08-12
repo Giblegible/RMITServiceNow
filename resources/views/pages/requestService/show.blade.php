49570d5f96adfba0da4752057697d30278446c6f
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-12 18:04:31 +1000

    Changed view structure

--- resources/views/pages/requestService/show.blade.php
+++ resources/views/pages/requestService/show.blade.php
@@ -2,27 +2,7 @@
 @section('title', 'View Tickets')
 @section('content')
     <div class="container">
-        <h2>Service Request</h2>
-        <hr>
-        <p>ID: {{ $ticket->id }}</p>
-        <p>Work Area: {{ $ticket->workArea}}</p>
-        <hr>
-        <p>Service Area: {{ $ticket->serviceArea}}</p>
-        <p>Hardware: {{ $ticket->hardwareType}}</p>
-        <p>Software: {{ $ticket->softwareType}}</p>
-        <hr>
-        <p>Description: {{ $ticket->problemDescription}}</p>
-        <p>Status: {{ $ticket->problemStatus }}</p>
-        <p>Severity: {{ $ticket->problemSeverity }}</p>
-        <p>Comments:</p>
-        @if($comments != null)
-            @foreach($comments as $comment)
-                <div class="panel panel-body">{{ $comment->comment }} - {{ $comment->created_at }}</div>
-            @endforeach
-        @else
-            <p>No comments</p>
-        @endif
-        <hr>
+        @include('includes.requestService.requestServiceShow')
         <a href="{{ URL::asset('pages/requestService/'.$ticket->id.'/edit') }}" class="btn btn-success">Update</a>
         <a href="{{ URL::to('pages/requestService') }}" class="btn btn-info">Back</a>
     </div>
