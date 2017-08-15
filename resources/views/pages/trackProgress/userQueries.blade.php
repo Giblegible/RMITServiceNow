62cd322278e4c9e97b726c1d55eab00780a889cd
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-15 13:03:29 +1000

    userQueries page to list searched users queries

--- resources/views/pages/trackProgress/userQueries.blade.php
+++ resources/views/pages/trackProgress/userQueries.blade.php
@@ -1,7 +1,50 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: James
- * Date: 15/8/17
- * Time: 12:42 PM
- */
+@extends('layout.master')
+@section('title', 'Track Progress')
+@section('content')
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col-md-1">
+                <h3>ID</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Service Area</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Work Area</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Description</h3>
+            </div>
+            <div class="col-md-2">
+                <h3>Status</h3>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+        <hr>
+        @foreach($tickets as $ticket)
+            <div class="row">
+                <div class="col-md-1">
+                    <p>{{ $ticket->id}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->serviceArea}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->workArea}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->problemDescription}}</p>
+                </div>
+                <div class="col-md-2">
+                    <p>{{ $ticket->problemStatus}}</p>
+                </div>
+                <div class="col-md-2">
+                    <a href="{{ URL::to('pages/admin/adminRequestService', $ticket->id) }}" class="btn btn-success">More info.</a>
+                </div>
+            </div>
+            <hr>
+        @endforeach
+    </div>
+@endsection
