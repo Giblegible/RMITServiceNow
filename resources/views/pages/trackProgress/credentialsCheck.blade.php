8cf8be30fcfdecc2df2f1d67ca52789f8d36c932
JamesMcLennan <jamesamclennan@gmail.com>
2017-08-15 13:03:07 +1000

    Errors and messages add to check

--- resources/views/pages/trackProgress/credentialsCheck.blade.php
+++ resources/views/pages/trackProgress/credentialsCheck.blade.php
@@ -3,6 +3,21 @@
 @section('content')
     <div class="container">
         <hr>
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if ($message = Session::get('fail'))
+            <div class="alert alert-danger">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
         <div class="row">
             <form action="{{ url('/{findEmail}') }}" method="get">
                 <div class="container">
