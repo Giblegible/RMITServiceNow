e7bdb73371fb7b0a7a1ca73bc0d983842d05f433
JamesMcLennan <jamesamclennan@gmail.com>
2017-09-09 20:07:33 +1000

    Updated API Admin Controller

--- app/Http/Controllers/AdminQueryController.php
+++ app/Http/Controllers/AdminQueryController.php
@@ -13,26 +13,22 @@ use App\CustomerQuery;
 
 class AdminQueryController extends Controller
 {
+    //API Method to return all tickets that exist.
     public function index()
     {
         return CustomerQuery::all();
     }
 
-    //Need to implement correct 'Fail' search.
-    public function filter(FilterRequest $request)
+    //API Method to return a customer, and all of their submitted tickets that exist.
+    public function filter($customer_id)
     {
-        $allRequest = $request->all();
-        Log::info('ADMIN - Search Request made for: '.$allRequest['emailText']);
-        $customer = Customer::all()->where('email', $allRequest['emailText'])->first();
-        if(is_null($customer))
-        {
-            Log::info('ADMIN - Search Request Failed: No customer found.');
-            $tickets = CustomerQuery::all();
-            return redirect('pages/admin/adminRequestService/')->with('fail', 'User does not exist')->with('tickets', $tickets);
-        }
-        return CustomerQuery::all()->where('customer_id', $customer->id);
+        Log::info('ADMIN - Search Request made for ID: '.$customer_id);
+        $tickets = CustomerQuery::all()->where('customer_id', $customer_id);
+        $customer = Customer::all()->where('id', $customer_id);
+        return response()->json([$customer, $tickets], 200);
     }
 
+    //API Method to display a specific ticket
     public function show($id) {
         $ticket = CustomerQuery::find($id);
         $comments = Comment::all()->where('conversation_id', $id);
@@ -40,12 +36,7 @@ class AdminQueryController extends Controller
         return response()->json([$ticket, $comments], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return $ticket and $comments
-     */
+    //API Method to update a specific ticket's information
     public function update(UpdateQueryRequest $request, $id)
     {
         $allRequest = $request->all();
