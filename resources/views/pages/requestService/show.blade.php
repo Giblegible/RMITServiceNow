eed2c9c825065c07cae44d78fc98f3a33af0a247
JamesMcLennan <jamesamclennan@gmail.com>
2017-09-10 14:19:36 +1000

    Update show and edit pages

--- resources/views/pages/requestService/show.blade.php
+++ resources/views/pages/requestService/show.blade.php
@@ -5,11 +5,11 @@
         @if(Auth::user()->email === $ticket->customer->email)
             @include('includes.requestService.requestServiceShow')
             <div class="container">
-            {!! Form::open(['action' => 'CustomerQueryController@getUserQueries']) !!}
+                {!! Form::open(['action' => 'CustomerQueryController@getUserQueries']) !!}
                 <a href="{{ URL::asset('pages/requestService/'.$ticket->id.'/edit') }}" class="btn btn-success">Update</a>
                 {!! Form::text('emailText', $ticket->customer->email, ['class' => 'form-control hidden']) !!}
                 <button class="btn btn-success" type="submit">Back</button>
-            {!! Form::close() !!}
+                {!! Form::close() !!}
             </div>
         @else
             <section id="action" class="responsive">
@@ -37,7 +37,7 @@
                 </div>
 
             </section>
-            <a href="{{ URL::to('trackProgress\credentialsCheck') }}" class="btn btn-warning">Track Progress</a>
+            <a href="{{ URL::to('pages/trackProgress/userQueries') }}" class="btn btn-warning">Track Progress</a>
         @endif
     </div>
 @endsection
